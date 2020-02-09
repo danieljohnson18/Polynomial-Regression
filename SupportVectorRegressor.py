ae8c440fc24889d88894b95e420a85b932c88795
Daniel Johnson <arikawedaniel@gmail.com>
2020-02-09 20:38:16 +0100

    Polynomial Regression

---
+++ SupportVectorRegressor.py
@@ -0,0 +1,49 @@
+#Support Vector Rgression
+
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd 
+
+#import the dataset
+dataset = pd.read_csv('Position_Salaries.csv')
+X = dataset.iloc[:, 1:2].values
+y = dataset.iloc[:, 2].values
+
+#splitting dataset into training and test set
+'''from sklearn.cross_validation import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)'''
+
+#feature scaling
+from sklearn.preprocessing import StandardScaler
+sc_X = StandardScaler()
+sc_y = StandardScaler()
+X = sc_X.fit_transform(X)
+y = sc_y.fit_transform(y)
+
+
+#Fitting the SVR Model to the Dataset
+from sklearn.svm import SVR
+regressor = SVR(kernel = 'rbf')
+regressor.fit(X, y)
+
+
+#Predicting a new Result with Regression
+y_pred = regressor.predict(6.5)
+
+#Visualizing the Regression Results
+plt.scatter(X, y, color = 'red')
+plt.plot(X, regressor.predict(X), color ='blue')
+plt.title('Truth or Bluff (Support Vector Regressor)')
+plt.xlabel('Position Level')
+plt.ylabel('Salary')
+plt.show()
+
+#Visualizing the Regression Results for higher resolution
+X_grid = np.arange(min(X), max(X), 0.1)
+X_grid = X_grid.reshape((len (X_grid), 1))
+plt.scatter(X, y, color = 'red')
+plt.plot(X_grid, regressor.predict(X_grid), color ='blue')
+plt.title('Truth or Bluff (Support Vector Regressor)')
+plt.xlabel('Position Level')
+plt.ylabel('Salary')
+plt.show()
